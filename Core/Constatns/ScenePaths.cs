de62a049c68bf34ac69c3eb0de2e6ac8458f7063
unknown <vankiselev@mail.ru>
2023-04-07 21:03:36 +0300

    add some infra

---
+++ Core/Constatns/ScenePaths.cs
@@ -0,0 +1,10 @@
+﻿namespace My_awesome_character.Core.Constatns
+{
+    internal class ScenePaths
+    {
+        public const string Player_Front = "";
+        public const string Player_Back = "";
+        public const string Player_Left = "";
+        public const string Player_Right = "";
+    }
+}
---
+++ Core/SceneFactory.cs
@@ -0,0 +1,21 @@
+﻿using Godot;
+using System;
+
+namespace My_awesome_character.Core
+{
+    public static class SceneFactory
+    {
+        public static T Create<T>(string name, string path) where T : class
+        {
+            var inventoryScene = (PackedScene)ResourceLoader.Load(path);
+            if (inventoryScene == null)
+                throw new ApplicationException($"Can't find scene \'{path}\'.");
+
+            var scene = inventoryScene.Instantiate();
+            scene.Name = name;
+            return scene is T 
+                ? scene as T 
+                : throw new ArgumentException($"can't cast scene ({path}) to type {typeof(T).Name} couse original type is {scene.GetType().Name} ");
+        }
+    }
+}
