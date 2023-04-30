2b584dc43ed2f27fc04b2532f7313953c5ebca97
unknown <vankiselev@mail.ru>
2023-04-30 22:33:27 +0300

    extend node search

--- Core/Ui/ISceneAccessor.cs
+++ Core/Ui/ISceneAccessor.cs
@@ -1,8 +1,16 @@
-﻿namespace My_awesome_character.Core.Ui
+﻿using System;
+using System.Collections.Generic;
+
+namespace My_awesome_character.Core.Ui
 {
     internal interface ISceneAccessor
     {
         T FindFirst<T>(string name) where T : class;
+
+        IEnumerable<T> FindAll<T>(Predicate<Godot.Node> predicate) where T : class;
+
+        IEnumerable<T> FindAll<T>() where T : class;
+
         T GetScene<T>(string name) where T : class;
     }
 }
--- Core/Ui/SceneAccessor.cs
+++ Core/Ui/SceneAccessor.cs
@@ -1,4 +1,6 @@
 ﻿using Godot;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace My_awesome_character.Core.Ui
@@ -22,5 +24,29 @@ namespace My_awesome_character.Core.Ui
                 .Select(c => Find(c, name))
                 .FirstOrDefault(c => c != null);
         }
+
+        public IEnumerable<T> FindAll<T>(Predicate<Node> predicate) where T : class
+        {
+            return Find(Root, predicate).Cast<T>();
+        }
+
+        public IEnumerable<T> FindAll<T>() where T : class
+        {
+            return Find(Root, n => n is T).Cast<T>();
+        }
+
+        private IEnumerable<Node> Find(Node current, Predicate<Node> predicate)
+        {
+            if (predicate(current))
+                yield return current;
+
+
+            foreach (var child in current.GetChildren().SelectMany(c => Find(c, predicate)))
+                yield return child;
+
+            yield break;
+        }
+
+        
     }
 }
