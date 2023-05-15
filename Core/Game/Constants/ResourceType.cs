02661f9bb3e8961658206e99e0b0521d1b436e82
unknown <vankiselev@mail.ru>
2023-05-15 21:31:58 +0300

    add constants for resources types

---
+++ Core/Game/Constants/ResourceType.cs
@@ -0,0 +1,12 @@
+﻿namespace My_awesome_character.Core.Game.Constants
+{
+    public static class ResourceType
+    {
+        public const int Money = 1;
+        public const int Water = 2;
+        public const int Food = 3;
+        public const int Electricity = 4;
+        public const int Steel = 5;
+        public const int Uranus = 6;
+    }
+}
