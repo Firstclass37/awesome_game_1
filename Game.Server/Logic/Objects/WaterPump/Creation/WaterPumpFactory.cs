57e50b8e73c5a773fa4ce23a392fa8526f32c10e
unknown <vankiselev@mail.ru>
2023-07-16 23:56:46 +0300

    add water pump

---
+++ Game.Server/Logic/Objects/WaterPump/Creation/WaterPumpFactory.cs
@@ -0,0 +1,20 @@
+﻿using Game.Server.DataBuilding;
+using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects.WaterPump.Interaction;
+using Game.Server.Models.Constants;
+using Game.Server.Models.GameObjects;
+using Game.Server.Models.Maps;
+
+namespace Game.Server.Logic.Objects.WaterPump.Creation
+{
+    internal class WaterPumpFactory : IGameObjectFactory
+    {
+        public GameObjectAggregator CreateNew(Coordiante root, Coordiante[] area)
+        {
+            return new GameObjectAggregatorBuilder(BuildingTypes.WaterPump)
+                .AddArea(root, area)
+                .AddInteraction<WaterPumpInteraction>()
+                .Build();
+        }
+    }
+}
---
+++ Game.Server/Logic/Objects/WaterPump/Metadata.cs
@@ -0,0 +1,33 @@
+﻿using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects._Core;
+using Game.Server.Logic.Objects._Requirements;
+using Game.Server.Logic.Objects.WaterPump.Creation;
+using Game.Server.Models.Constants;
+using Game.Server.Models.Resources;
+
+namespace Game.Server.Logic.Objects.WaterPump
+{
+    internal class Metadata : IGameObjectMetadata
+    {
+        private readonly IArea2x2Getter _area2x2Getter;
+        private readonly WaterPumpFactory _waterPumpFactory;
+
+        public Metadata(IArea2x2Getter area2x2Getter, WaterPumpFactory waterPumpFactory)
+        {
+            _area2x2Getter = area2x2Getter;
+            _waterPumpFactory = waterPumpFactory;
+        }
+
+        public string ObjectType => BuildingTypes.WaterPump;
+
+        public string Description => "Water pump";
+
+        public IAreaGetter AreaGetter => _area2x2Getter;
+
+        public ICreationRequirement CreationRequirement => new OnlyTypeRequirement(ResourceResourceTypes.Groundwater);
+
+        public IGameObjectFactory GameObjectFactory => _waterPumpFactory;
+
+        public Price BasePrice => Price.Create(new ResourceChunk(ResourceType.Steel, 10));
+    }
+}
