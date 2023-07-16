f2012fc0e7c26242f326dfa7e6998e493bd2c4cf
unknown <vankiselev@mail.ru>
2023-07-16 21:59:38 +0300

    add glass factory

---
+++ Game.Server/Logic/Objects/GlassFactory/Creation/GlassFactoryFactory.cs
@@ -0,0 +1,20 @@
+﻿using Game.Server.DataBuilding;
+using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects.GlassFactory.Interaction;
+using Game.Server.Models.Constants;
+using Game.Server.Models.GameObjects;
+using Game.Server.Models.Maps;
+
+namespace Game.Server.Logic.Objects.GlassFactory.Creation
+{
+    internal class GlassFactoryFactory : IGameObjectFactory
+    {
+        public GameObjectAggregator CreateNew(Coordiante root, Coordiante[] area)
+        {
+            return new GameObjectAggregatorBuilder(BuildingTypes.GlassFactory)
+                .AddArea(root, area)
+                .AddInteraction<GlassFactoryInteraction>()
+                .Build();
+        }
+    }
+}
---
+++ Game.Server/Logic/Objects/GlassFactory/Metadata.cs
@@ -0,0 +1,35 @@
+﻿using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects._Core;
+using Game.Server.Logic.Objects._Requirements;
+using Game.Server.Logic.Objects.GlassFactory.Creation;
+using Game.Server.Models.Constants;
+using Game.Server.Models.Resources;
+
+namespace Game.Server.Logic.Objects.GlassFactory
+{
+    internal class Metadata : IGameObjectMetadata
+    {
+        private readonly IArea2x2Getter _area2x2Getter;
+        private readonly GlassFactoryFactory _glassFactoryFactory;
+
+        public Metadata(IArea2x2Getter area2x2Getter, GlassFactoryFactory glassFactoryFactory)
+        {
+            _area2x2Getter = area2x2Getter;
+            _glassFactoryFactory = glassFactoryFactory;
+        }
+
+        public string ObjectType => BuildingTypes.GlassFactory;
+
+        public string Description => "Glass Factory";
+
+        public IAreaGetter AreaGetter => _area2x2Getter;
+
+        public ICreationRequirement CreationRequirement => new OnlyGroundRequirement();
+
+        public IGameObjectFactory GameObjectFactory => _glassFactoryFactory;
+
+        public Price BasePrice => Price.Create(
+            new ResourceChunk(ResourceType.Steel, 30),
+            new ResourceChunk(ResourceType.Silicon, 20));
+    }
+}
