cf36adb647e7e271dd2c640c6f7d07ec34547e0c
unknown <vankiselev@mail.ru>
2023-07-16 21:23:51 +0300

    add coal mine

---
+++ Game.Server/Logic/Objects/CoalMine/Creation/CoalMineFactory.cs
@@ -0,0 +1,20 @@
+﻿using Game.Server.DataBuilding;
+using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects.CoalMine.Interaction;
+using Game.Server.Models.Constants;
+using Game.Server.Models.GameObjects;
+using Game.Server.Models.Maps;
+
+namespace Game.Server.Logic.Objects.CoalMine.Creation
+{
+    internal class CoalMineFactory : IGameObjectFactory
+    {
+        public GameObjectAggregator CreateNew(Coordiante root, Coordiante[] area)
+        {
+            return new GameObjectAggregatorBuilder(BuildingTypes.CoalMine)
+                .AddArea(root, area)
+                .AddInteraction<CoalMineInteraction>()
+                .Build();
+        }
+    }
+}
---
+++ Game.Server/Logic/Objects/CoalMine/Metadata.cs
@@ -0,0 +1,33 @@
+﻿using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects._Core;
+using Game.Server.Logic.Objects._Requirements;
+using Game.Server.Logic.Objects.CoalMine.Creation;
+using Game.Server.Models.Constants;
+using Game.Server.Models.Resources;
+
+namespace Game.Server.Logic.Objects.CoalMine
+{
+    internal class Metadata : IGameObjectMetadata
+    {
+        private readonly IArea2x2Getter _area2x2Getter;
+        private readonly CoalMineFactory _coalMineFactory;
+
+        public Metadata(IArea2x2Getter area2x2Getter, CoalMineFactory coalMineFactory)
+        {
+            _area2x2Getter = area2x2Getter;
+            _coalMineFactory = coalMineFactory;
+        }
+
+        public string ObjectType => BuildingTypes.CoalMine;
+
+        public string Description => "Coal Mine";
+
+        public IAreaGetter AreaGetter => _area2x2Getter;
+
+        public ICreationRequirement CreationRequirement => new OnlyTypeRequirement(ResourceResourceTypes.Coke);
+
+        public IGameObjectFactory GameObjectFactory => _coalMineFactory;
+
+        public Price BasePrice => new Price(new ResourceChunk(ResourceType.Steel, 45));
+    }
+}
