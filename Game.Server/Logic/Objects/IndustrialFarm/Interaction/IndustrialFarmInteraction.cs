77c35785123614119463cb6c0f576ecb1b0ac735
unknown <vankiselev@mail.ru>
2023-07-16 22:12:41 +0300

    add Industrial farm

---
+++ Game.Server/Logic/Objects/IndustrialFarm/Interaction/IndustrialFarmInteraction.cs
@@ -0,0 +1,18 @@
+﻿using Game.Server.Logic.Objects._Interactions;
+using Game.Server.Logic.Objects.Characters;
+using Game.Server.Logic.Resources;
+using Game.Server.Models.Constants;
+
+namespace Game.Server.Logic.Objects.IndustrialFarm.Interaction
+{
+    internal class IndustrialFarmInteraction : InteractableBuildingInteraction
+    {
+        public IndustrialFarmInteraction(IResourceManager resourceManager, ICharacterDamageService characterDamageService) : base(resourceManager, characterDamageService)
+        {
+            AddRequiredResource(ResourceType.Energy, 1);
+            AddRequiredResource(ResourceType.Chemicals, 0.1f);
+
+            AddTargetResource(ResourceType.Food, 2);
+        }
+    }
+}
---
+++ Game.Server/Logic/Objects/IndustrialFarm/Metadata.cs
@@ -0,0 +1,35 @@
+﻿using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects._Core;
+using Game.Server.Logic.Objects._Requirements;
+using Game.Server.Logic.Objects.IndustrialFarm.Creation;
+using Game.Server.Models.Constants;
+using Game.Server.Models.Resources;
+
+namespace Game.Server.Logic.Objects.IndustrialFarm
+{
+    internal class Metadata : IGameObjectMetadata
+    {
+        private readonly IndustrialFarmFactory _industrialFarmFactory;
+        private readonly IArea2x2Getter _area2x2Getter;
+
+        public Metadata(IndustrialFarmFactory industrialFarmFactory, IArea2x2Getter area2x2Getter)
+        {
+            _industrialFarmFactory = industrialFarmFactory;
+            _area2x2Getter = area2x2Getter;
+        }
+
+        public string ObjectType => BuildingTypes.IndustrialFarm;
+
+        public string Description => "Industrial farm";
+
+        public IAreaGetter AreaGetter => _area2x2Getter;
+
+        public ICreationRequirement CreationRequirement => new OnlyGroundRequirement();
+
+        public IGameObjectFactory GameObjectFactory => _industrialFarmFactory;
+
+        public Price BasePrice => Price.Create(
+            new ResourceChunk(ResourceType.Steel, 25),
+            new ResourceChunk(ResourceType.Glass, 10));
+    }
+}
