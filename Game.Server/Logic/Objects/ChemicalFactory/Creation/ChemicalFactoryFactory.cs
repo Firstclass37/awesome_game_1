4836b919f0f12558bab5c68e1c2ebef07f40ab29
unknown <vankiselev@mail.ru>
2023-07-16 21:18:00 +0300

    add chemical factory

---
+++ Game.Server/Logic/Objects/ChemicalFactory/Creation/ChemicalFactoryFactory.cs
@@ -0,0 +1,20 @@
+﻿using Game.Server.DataBuilding;
+using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects.ChemicalFactory.Interaction;
+using Game.Server.Models.Constants;
+using Game.Server.Models.GameObjects;
+using Game.Server.Models.Maps;
+
+namespace Game.Server.Logic.Objects.ChemicalFactory.Creation
+{
+    internal class ChemicalFactoryFactory : IGameObjectFactory
+    {
+        public GameObjectAggregator CreateNew(Coordiante root, Coordiante[] area)
+        {
+            return new GameObjectAggregatorBuilder(BuildingTypes.ChemicalFactory)
+                .AddArea(root, area)
+                .AddInteraction<ChemicalFactoryInteraction>()
+                .Build();
+        }
+    }
+}
---
+++ Game.Server/Logic/Objects/ChemicalFactory/Metadata.cs
@@ -0,0 +1,35 @@
+﻿using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects._Core;
+using Game.Server.Logic.Objects._Requirements;
+using Game.Server.Logic.Objects.ChemicalFactory.Creation;
+using Game.Server.Models.Constants;
+using Game.Server.Models.Resources;
+
+namespace Game.Server.Logic.Objects.ChemicalFactory
+{
+    internal class Metadata : IGameObjectMetadata
+    {
+        private readonly IArea2x2Getter _area2X2Getter;
+        private readonly ChemicalFactoryFactory _chemicalFactoryFactory;
+
+        public Metadata(IArea2x2Getter area2X2Getter, ChemicalFactoryFactory chemicalFactoryFactory)
+        {
+            _area2X2Getter = area2X2Getter;
+            _chemicalFactoryFactory = chemicalFactoryFactory;
+        }
+
+        public string ObjectType => BuildingTypes.ChemicalFactory;
+
+        public string Description => "Chemical Factory";
+
+        public IAreaGetter AreaGetter => _area2X2Getter;
+
+        public ICreationRequirement CreationRequirement => new OnlyTypeRequirement(BuildingTypes.Ground);
+
+        public IGameObjectFactory GameObjectFactory => _chemicalFactoryFactory;
+
+        public Price BasePrice => Price.Create(
+            new ResourceChunk(ResourceType.Steel, 30),
+            new ResourceChunk(ResourceType.Coal, 20));
+    }
+}
