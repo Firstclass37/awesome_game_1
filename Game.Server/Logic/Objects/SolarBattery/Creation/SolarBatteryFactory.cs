f5a15c553dae7cad0659ae998747c7722b7e8c8d
unknown <vankiselev@mail.ru>
2023-07-16 23:51:19 +0300

    add solar battery

---
+++ Game.Server/Logic/Objects/SolarBattery/Creation/SolarBatteryFactory.cs
@@ -0,0 +1,20 @@
+﻿using Game.Server.DataBuilding;
+using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects.SolarBattery.Interaction;
+using Game.Server.Models.Constants;
+using Game.Server.Models.GameObjects;
+using Game.Server.Models.Maps;
+
+namespace Game.Server.Logic.Objects.SolarBattery.Creation
+{
+    internal class SolarBatteryFactory : IGameObjectFactory
+    {
+        public GameObjectAggregator CreateNew(Coordiante root, Coordiante[] area)
+        {
+            return new GameObjectAggregatorBuilder(BuildingTypes.SolarBattery)
+                .AddArea(root, area)
+                .AddInteraction<SolarBatteryInteraction>()
+                .Build();
+        }
+    }
+}
---
+++ Game.Server/Logic/Objects/SolarBattery/Metadata.cs
@@ -0,0 +1,33 @@
+﻿using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects._Core;
+using Game.Server.Logic.Objects._Requirements;
+using Game.Server.Logic.Objects.SolarBattery.Creation;
+using Game.Server.Models.Constants;
+using Game.Server.Models.Resources;
+
+namespace Game.Server.Logic.Objects.SolarBattery
+{
+    internal class Metadata : IGameObjectMetadata
+    {
+        private readonly IArea2x2Getter _area2x2Getter;
+        private readonly SolarBatteryFactory _solarBatteryFactory;
+
+        public Metadata(IArea2x2Getter area2x2Getter, SolarBatteryFactory solarBatteryFactory)
+        {
+            _area2x2Getter = area2x2Getter;
+            _solarBatteryFactory = solarBatteryFactory;
+        }
+
+        public string ObjectType => BuildingTypes.SolarBattery;
+
+        public string Description => "Solar battery";
+
+        public IAreaGetter AreaGetter => _area2x2Getter;
+
+        public ICreationRequirement CreationRequirement => new OnlyGroundRequirement();
+
+        public IGameObjectFactory GameObjectFactory => _solarBatteryFactory;
+
+        public Price BasePrice => Price.Create(new ResourceChunk(ResourceType.Aluminum, 20), new ResourceChunk(ResourceType.Silicon, 100));
+    }
+}
