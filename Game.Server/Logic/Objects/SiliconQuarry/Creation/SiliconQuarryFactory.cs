c33c7f1d1048bab6b4f44f67803f2b429dc4df1e
unknown <vankiselev@mail.ru>
2023-07-16 22:37:19 +0300

    add silicon quarry

---
+++ Game.Server/Logic/Objects/SiliconQuarry/Creation/SiliconQuarryFactory.cs
@@ -0,0 +1,20 @@
+﻿using Game.Server.DataBuilding;
+using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects.SiliconQuarry.Interaction;
+using Game.Server.Models.Constants;
+using Game.Server.Models.GameObjects;
+using Game.Server.Models.Maps;
+
+namespace Game.Server.Logic.Objects.SiliconQuarry.Creation
+{
+    internal class SiliconQuarryFactory : IGameObjectFactory
+    {
+        public GameObjectAggregator CreateNew(Coordiante root, Coordiante[] area)
+        {
+            return new GameObjectAggregatorBuilder(BuildingTypes.SiliconQuarry)
+                .AddArea(root, area)
+                .AddInteraction<SiliconQuarryInteraction>()
+                .Build();
+        }
+    }
+}
---
+++ Game.Server/Logic/Objects/SiliconQuarry/Metadata.cs
@@ -0,0 +1,33 @@
+﻿using Game.Server.Logic.Objects._Buidling;
+using Game.Server.Logic.Objects._Core;
+using Game.Server.Logic.Objects._Requirements;
+using Game.Server.Logic.Objects.SiliconQuarry.Creation;
+using Game.Server.Models.Constants;
+using Game.Server.Models.Resources;
+
+namespace Game.Server.Logic.Objects.SiliconQuarry
+{
+    internal class Metadata : IGameObjectMetadata
+    {
+        private readonly IArea2x2Getter _area2x2Getter;
+        private readonly SiliconQuarryFactory _siliconQuarryFactory;
+
+        public Metadata(IArea2x2Getter area2x2Getter, SiliconQuarryFactory siliconQuarryFactory)
+        {
+            _area2x2Getter = area2x2Getter;
+            _siliconQuarryFactory = siliconQuarryFactory;
+        }
+
+        public string ObjectType => BuildingTypes.SiliconQuarry;
+
+        public string Description => "Silicon quarry";
+
+        public IAreaGetter AreaGetter => _area2x2Getter;
+
+        public ICreationRequirement CreationRequirement => new OnlyTypeRequirement(ResourceResourceTypes.Minerals);
+
+        public IGameObjectFactory GameObjectFactory => _siliconQuarryFactory;
+
+        public Price BasePrice => Price.Create(new ResourceChunk(ResourceType.Steel, 45));
+    }
+}
