e31dde7822377aab84f95d2c06ba45b3bebc6fc3
unknown <vankiselev@mail.ru>
2023-07-30 18:39:46 +0300

    traffic light factory refactoring

--- Game.Server/Logic/Objects/TrafficLights/Creation/TrafficLightFactory.cs
+++ Game.Server/Logic/Objects/TrafficLights/Creation/TrafficLightFactory.cs
@@ -1,7 +1,7 @@
-﻿using Game.Server.Logic.Maps;
+﻿using Game.Server.DataBuilding;
+using Game.Server.Logic.Maps;
 using Game.Server.Logic.Objects._Buidling;
 using Game.Server.Logic.Objects.TrafficLights.Interaction;
-using Game.Server.Models;
 using Game.Server.Models.Constants;
 using Game.Server.Models.GameObjects;
 using Game.Server.Models.Maps;
@@ -25,27 +25,14 @@ namespace Game.Server.Logic.Objects.TrafficLights.Creation
             var readNeigbors = neighbors.Where(n => _gameObjectAccessor.Find(n.Key)?.GameObject.ObjectType == BuildingTypes.Road)
                 .ToDictionary(n => n.Value, n => n.Key);
 
-            var gameObject = new GameObject(BuildingTypes.TrafficLigh);
-            var positions = area.Select(a => new GameObjectPosition(gameObject.Id, root, a.Equals(root))).ToList();
-            var interactions = new List<GameObjectInteraction>()
-            {
-                new GameObjectInteraction(gameObject.Id, typeof(TrafficLightInteraction).FullName)
-            };
-
-            var attributes = new List<GameObjectToAttribute>()
-            {
-                new GameObjectToAttribute(gameObject.Id, AttributeType.TrafficLightTrackingCells, readNeigbors),
-                new GameObjectToAttribute(gameObject.Id, AttributeType.TrafficLightSidesCapacity, neighbors.Select(d => d.Value).ToDictionary(d => d, d => 1)),
-                new GameObjectToAttribute(gameObject.Id, AttributeType.TrafficLightSidesValues, neighbors.Select(d => d.Value).ToDictionary(d => d, d => 0))
-            };
-
-            return new GameObjectAggregator
-            {
-                GameObject = gameObject,
-                Area = positions,
-                Attributes = attributes,
-                Interactions = interactions,
-            };
+            return new GameObjectAggregatorBuilder(BuildingTypes.TrafficLigh)
+                .AddArea(root, area)
+                .AddAttribute(AttrituteTypes.Interactable)
+                .AddAttribute(AttributeType.TrafficLightTrackingCells, readNeigbors)
+                .AddAttribute(AttributeType.TrafficLightSidesCapacity, neighbors.Select(d => d.Value).ToDictionary(d => d, d => 1))
+                .AddAttribute(AttributeType.TrafficLightSidesValues, neighbors.Select(d => d.Value).ToDictionary(d => d, d => 0))
+                .AddInteraction<TrafficLightInteraction>()
+                .Build();
         }
     }
 }
