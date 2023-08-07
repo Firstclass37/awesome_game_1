7ab10bbc58535cfaefa4d41991ac2168e9b1f08f
unknown <vankiselev@mail.ru>
2023-08-07 21:00:31 +0300

    now we can buy buidling for base price

--- Game.Server/API/Buildings/BuildingController.cs
+++ Game.Server/API/Buildings/BuildingController.cs
@@ -1,11 +1,22 @@
 ﻿using Game.Server.Logic.Objects._Buidling;
 using Game.Server.Logic.Objects._Core;
+using Game.Server.Logic.Resources;
 using Game.Server.Models.Constants;
 using Game.Server.Models.Maps;
 
 namespace Game.Server.API.Buildings
 {
-    public record BuildingInfo(string BuildingType, string Description, Price[] Prices);
+    public record BuildingInfo
+    {
+        public string BuildingType { get; init; }
+
+        public string Description { get; init; }
+
+        public Price[] Prices { get; init; }
+
+        public bool CanBuild { get; init; }
+    
+    };
 
     public record Price(int resourceType, float count);
 
@@ -13,11 +24,13 @@ namespace Game.Server.API.Buildings
     {
         private readonly IGameObjectMetadataCollection _gameObjectMetadataCollection;
         private readonly IGameObjectCreator _gameObjectCreator;
+        private readonly ISpendingTransactionFactory _spendingTransactionFactory;
 
-        public BuildingController(IGameObjectMetadataCollection gameObjectMetadataCollection, IGameObjectCreator gameObjectCreator)
+        public BuildingController(IGameObjectMetadataCollection gameObjectMetadataCollection, IGameObjectCreator gameObjectCreator, ISpendingTransactionFactory spendingTransactionFactory)
         {
             _gameObjectMetadataCollection = gameObjectMetadataCollection;
             _gameObjectCreator = gameObjectCreator;
+            _spendingTransactionFactory = spendingTransactionFactory;
         }
 
         public IReadOnlyCollection<BuildingInfo> GetBuildableList()
@@ -30,10 +43,14 @@ namespace Game.Server.API.Buildings
                     BuildingTypes.TrafficLigh
                 })
             .Select(t => _gameObjectMetadataCollection.Get(t))
-            .Select(m => new BuildingInfo(m.ObjectType, m.Description, m.BasePrice.Chunks.Select(p => new Price(p.ResourceId, p.Amout)).ToArray()))
+            .Select(m => new BuildingInfo 
+            {
+                BuildingType = m.ObjectType,
+                Description = m.Description,
+                Prices = m.BasePrice.Chunks.Select(p => new Price(p.ResourceId, p.Amout)).ToArray()
+            })
             .ToArray();
         }
-
 
         public bool CanBuild(string buildingType, Coordiante point)
         {
@@ -50,6 +67,15 @@ namespace Game.Server.API.Buildings
             {
                 //
             }
+        }
+
+        public void Buy(string buildingType, Coordiante point)
+        {
+            var price = _gameObjectMetadataCollection.Get(buildingType);
+            using var transaction = _spendingTransactionFactory.Create();
+            transaction.Spend(price.BasePrice.Chunks);
+            _gameObjectCreator.Create(buildingType, point, null);
+            transaction.Commit();
         }
     }
 }
---
+++ Game.Server/Logic/Resources/ISpendingTransaction.cs
@@ -0,0 +1,76 @@
+﻿using Game.Server.Models.Resources;
+
+namespace Game.Server.Logic.Resources
+{
+    internal interface ISpendingTransactionFactory
+    {
+        ISpendingTransaction Create();
+    }
+
+    internal class SpendingTransactionFactory : ISpendingTransactionFactory
+    {
+        private readonly IResourceManager _resourceManager;
+
+        public SpendingTransactionFactory(IResourceManager resourceManager)
+        {
+            _resourceManager = resourceManager;
+        }
+
+        public ISpendingTransaction Create() => new SpendingTransaction(_resourceManager);
+    }
+
+    internal interface ISpendingTransaction: IDisposable
+    {
+        void Spend(IReadOnlyCollection<ResourceChunk> price);
+
+        void Commit();
+    }
+
+    internal class SpendingTransaction : ISpendingTransaction
+    {
+        private readonly IResourceManager _resourceManager;
+        private bool _commited = false;
+        private readonly List<ResourceChunk> _spentResources = new();
+
+        internal SpendingTransaction(IResourceManager resourceManager)
+        {
+            _resourceManager = resourceManager;
+        }
+
+        public void Spend(IReadOnlyCollection<ResourceChunk> prices)
+        {
+            try 
+            {
+                foreach (var price in prices)
+                {
+                    if (!_resourceManager.TrySpend(price.ResourceId, price.Amout)) 
+                        throw new ArgumentException($"not enough resources");
+
+                    _spentResources.Add(price);
+                }
+            }
+            catch
+            {
+                Rollback();
+                throw;
+            }
+        }
+
+        public void Commit()
+        {
+            _commited = true;
+        }
+
+        public void Dispose()
+        {
+            if (!_commited)
+                Rollback();
+        }
+
+        private void Rollback()
+        {
+            foreach (var price in _spentResources)
+                _resourceManager.Increase(price.ResourceId, price.Amout);
+        }
+    }
+}
