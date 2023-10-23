c6245020d5f9361286134f0997cf0b123f7e86f7
unknown <vankiselev@mail.ru>
2023-10-23 23:44:56 +0300

    now road width is 1 cell

--- Game.Server/Logic/Objects/Roads/Reuirements/RoadCreationRequirement.cs
+++ Game.Server/Logic/Objects/Roads/Reuirements/RoadCreationRequirement.cs
@@ -3,20 +3,23 @@ using Game.Server.Logic.Objects._Requirements;
 using Game.Server.Models.Constants;
 using Game.Server.Models.GameObjects;
 using Game.Server.Models.Maps;
-using Game.Server.Storage;
 
 namespace Game.Server.Logic.Objects.Roads.Reuirements
 {
     internal class RoadCreationRequirement : ICreationRequirement
     {
         private readonly IMapGrid _mapGrid;
-        private readonly IStorage _storage;
         private readonly IGameObjectAccessor _gameObjectAccessor;
 
-        public RoadCreationRequirement(IMapGrid mapGrid, IStorage storage, IGameObjectAccessor gameObjectAccessor)
+        private readonly HashSet<string> _occupingBuldingTypes = new HashSet<string>() 
+        {
+            BuildingTypes.Road,
+            BuildingTypes.TrafficLigh // Добавлен, т.к. у светофора площадь включает в себя трекаемые клетки
+        };
+
+        public RoadCreationRequirement(IMapGrid mapGrid, IGameObjectAccessor gameObjectAccessor)
         {
             _mapGrid = mapGrid;
-            _storage = storage;
             _gameObjectAccessor = gameObjectAccessor;
         }
 
@@ -26,13 +29,53 @@ namespace Game.Server.Logic.Objects.Roads.Reuirements
             if (root.Value == null || root.Value.GameObject.ObjectType != BuildingTypes.Ground)
                 return false;
 
-            var neigtborsCoordinates = _mapGrid.GetNeightborsOf(root.Key).Select(c => c.Key).ToArray();
-            var roadNeigtbors = neigtborsCoordinates
-                .Select(n => _gameObjectAccessor.Find(n))
-                .Where(n => n.GameObject.ObjectType == BuildingTypes.Road)
-                .ToArray();
+            var roadNeigtbors = _mapGrid.GetNeightborsOf(root.Key)
+                .Where(n => _occupingBuldingTypes.Contains(_gameObjectAccessor.Find(n.Key).GameObject.ObjectType))
+                .ToDictionary(n => n.Key, n => n.Value);
 
-            return roadNeigtbors.Length <= 2;
+            return WillBeInAngle(coordiante, roadNeigtbors) == false;
+        }
+
+        /// <summary>
+        /// 0 0  1 0
+        /// 1 0  0 0 
+        /// </summary>
+        
+        private bool WillBeInAngle(Coordiante coordiante, Dictionary<Coordiante, Direction> roadNeighbours)
+        {
+            if (roadNeighbours.Count <= 1)
+                return false;
+
+            var pairs = new List<Direction[]>()
+            {
+                new Direction[]{ Direction.Left, Direction.Top },
+                new Direction[]{ Direction.Left, Direction.Bottom },
+                new Direction[]{ Direction.Right, Direction.Top },
+                new Direction[]{ Direction.Right, Direction.Bottom }
+            };
+
+            foreach(var pair in pairs)
+            {
+                if (!pair.All(d => roadNeighbours.Values.Contains(d)))
+                    continue;
+
+                var firstStepDirection = pair[0];
+                var secondStepDirection = pair[1];
+
+                var firstNeightbour = _mapGrid.GetNeightborsOf(coordiante).Where(n => n.Value == firstStepDirection).FirstOrDefault().Key;
+                if (firstNeightbour == null)
+                    continue;
+
+                var secondNeightbour = _mapGrid.GetNeightborsOf(firstNeightbour).Where(n => n.Value == secondStepDirection).FirstOrDefault().Key;
+                if (secondNeightbour == null)
+                    continue;
+
+                var diagonalNeigbour = _gameObjectAccessor.Find(secondNeightbour);
+                if (diagonalNeigbour != null && _occupingBuldingTypes.Contains(diagonalNeigbour.GameObject.ObjectType))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
